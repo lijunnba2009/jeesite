36b55f8cb0144846e787020589d43edfbb9ead3f
lijunnba2009 <lijunnba2009@163.com>
2019-01-16 16:12:55 +0800

    1.导入项去除

--- src/main/java/com/thinkgem/jeesite/modules/learning/web/GalleryController.java
+++ src/main/java/com/thinkgem/jeesite/modules/learning/web/GalleryController.java
@@ -2,24 +2,8 @@ package com.thinkgem.jeesite.modules.learning.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.thinkgem.jeesite.common.persistence.Page;
-import com.thinkgem.jeesite.common.utils.StringUtils;
-import com.thinkgem.jeesite.modules.gen.entity.GenScheme;
-import com.thinkgem.jeesite.modules.gen.service.GenSchemeService;
-import com.thinkgem.jeesite.modules.gen.service.GenTableService;
-import com.thinkgem.jeesite.modules.gen.util.GenUtils;
-import com.thinkgem.jeesite.modules.sys.entity.User;
-import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
 
 @Controller
 @RequestMapping(value = "${adminPath}/learning/gallery")
